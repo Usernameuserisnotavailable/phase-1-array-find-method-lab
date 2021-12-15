fd6270d5b0969cab95fc6fd07dd4405e3a43da46
Jose Garcia <rocketrito1@gmail.com>
2021-12-15 14:29:52 -0800

    commit tree af08da8a63a0a1689297243c76d2e47baef9be33

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function superbowlWin(record) {
+  const firstWin = record.find(function(element)  {
+    return element.result === "W"
+  })
+  console.log(firstWin);
 
+
+const firstWinYear = record.find(function(element)  {
+  return element.result === "W"
+})
+return firstWinYear.year
+}
+// console.log(firstWin) 
+// return !!firstWin.year ? firstWin.year : firstWin
+
+// return !! firstWin ? firstWin.year : firstWin
+
+function superbowlWin(record) {
+  const game = record.find(gameWon)
+  if(game) {
+    return game.year
+  }
+}
+function gameWon(game){
+  return game.result === "W";
+}
